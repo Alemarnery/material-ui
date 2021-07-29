ffe57780a84e4e4915d46fc0c9223782b0c13715
Alemarnery <aleemar.95@gmail.com>
2021-07-29 16:42:48 -0500

    Icon

--- src/Components/IconComponent.jsx
+++ src/Components/IconComponent.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { AccessAlarm, ThreeDRotation, DeleteOutlined } from '@material-ui/icons';
+import { Typography, Button } from '@material-ui/core'
 
 
 const IconComponent = () => {
     return (
         <div>
-            <h2>Iconos</h2>
+            <Typography variant="h3" color="initial">Icons</Typography>
+            <AccessAlarm color="primary"   />
+            <ThreeDRotation/>
+            <Button variant="contained" color="secondary" endIcon={         <DeleteOutlined/> }    >Delete
+            </Button>
         </div>
     )
 }
