93cc91ef1de2575af33eafc5c90846e0abc3d66c
Alemarnery <aleemar.95@gmail.com>
2021-07-29 14:17:08 -0500

    button

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Button, ButtonGroup } from '@material-ui/core'
+
+function App() {
+  return (
+    <div>
+      <ButtonGroup variant="text" color="default" aria-label="">
+        <Button>Boton </Button>
+        <Button>boton</Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default App;
